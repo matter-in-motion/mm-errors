afaf3305f138b19399645e6d6b937352f096ceb0
velocityzen <velocityzen@gmail.com>
2016-05-02 21:41:32 -0400

    Added SubscriptionNotFound error

--- errors.js
+++ errors.js
@@ -23,32 +23,33 @@ let mmError = function(code, msg, toString) {
 }
 
 //generic error
-const Call                = mmError(4000, 'Error'); //general error to return from your methods
+const Call                  = mmError(4000, 'Error'); //general error to return from your methods
 
 //auth errors
-const Unauthorized        = mmError(4100, 'Unauthorized');
-const ProviderNotFound    = mmError(4104, 'Auth provider not found');
-const Forbidden           = mmError(4110, 'Forbidden');
-const NotValidToken       = mmError(4120, 'Token not valid or expired');
+const Unauthorized          = mmError(4100, 'Unauthorized');
+const ProviderNotFound      = mmError(4104, 'Auth provider not found');
+const Forbidden             = mmError(4110, 'Forbidden');
+const NotValidToken         = mmError(4120, 'Token not valid or expired');
 
 //validation errors
-const RequestValidation   = mmError(4200, 'Request validation failed');
-const ResponseValidation  = mmError(4210, 'Response validation failed');
-const RequestTooLarge     = mmError(4220, 'Request entity too large');
-const RequestDecode       = mmError(4230, 'Request decode error');
-const ResponseEncode      = mmError(4240, 'Response encode error');
-const UnsupportedMedia    = mmError(4250, 'Unsupported media');
+const RequestValidation     = mmError(4200, 'Request validation failed');
+const ResponseValidation    = mmError(4210, 'Response validation failed');
+const RequestTooLarge       = mmError(4220, 'Request entity too large');
+const RequestDecode         = mmError(4230, 'Request decode error');
+const ResponseEncode        = mmError(4240, 'Response encode error');
+const UnsupportedMedia      = mmError(4250, 'Unsupported media');
 
 //call errors
-const MethodNotFound      = mmError(4400, 'Method not found');
-const ChannelNotFound     = mmError(4410, 'Channel not found');
+const MethodNotFound        = mmError(4400, 'Method not found');
+const ChannelNotFound       = mmError(4410, 'Channel not found');
+const SubscriptionNotFound  = mmError(4420, 'Subscription not found');
 
 //response errors
-const Duplicate           = mmError(4500, 'Duplicate entity');
-const NotFound            = mmError(4540, 'Not found');
+const Duplicate             = mmError(4500, 'Duplicate entity');
+const NotFound              = mmError(4540, 'Not found');
 
 //server errors
-const ServerError         = mmError(5000, 'Server error');
+const ServerError           = mmError(5000, 'Server error');
 
 module.exports = {
   Error: mmError,
